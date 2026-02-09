3a4866403f8d25e5aeb3166daa8da78e391db065
Brack Allen <brack@Bracks-Air.lan>
2026-02-08 18:23:12 -0600

    add asnwers button

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class App(TkinterDnD.Tk):
         self.label = tk.Label(
             self,
             text="Drag Test PDF Here",
-            font=("Arial", 14),
+            font=("Arial", 18),
             relief="ridge",
             width=30,
             height=6
@@ -27,18 +27,27 @@ class App(TkinterDnD.Tk):
         self.label.drop_target_register(DND_FILES)
         self.label.dnd_bind("<<Drop>>", self.drop)
 
-        self.copy_button = tk.Button(self, text="Copy ",font=("Arial", 20), height=3, width=10, command=self.copy_to_clipboard)
+        self.copy_button = tk.Button(self, text="Copy Questions",font=("Arial", 23), height=3, width=13, command=lambda:self.copy_to_clipboard(FinalText, "questions"))
+        self.copy_answers_button = tk.Button(self, text="Copy Answers ",font=("Arial", 23), height=3, width=13, command=lambda:self.copy_to_clipboard(AnswersText, "answers"))
 
-    def copy_to_clipboard(self):
+    def copy_to_clipboard(self, copytext, type):
         self.clipboard_clear()
-        self.clipboard_append(FinalText)
+        self.clipboard_append(copytext)
         self.update()
-        self.copy_button.config(text="Copied")
+        if type == "questions":
+            self.copy_button.config(text="Copied Questions")
+            self.copy_answers_button.config(text="Copy Answers")
+        elif type == "answers":
+            self.copy_answers_button.config(text="Copied Answers")
+            self.copy_button.config(text="Copy Questions")
 
     def drop(self, event):
         global FinalText
         FinalText = ""
-        self.copy_button.config(text="Copy")
+        global AnswersText
+        AnswersText = ""
+        self.copy_button.config(text="Copy Questions")
+        self.copy_answers_button.config(text="Copy Answers")
 
         file_path = event.data.strip("{}")
 
@@ -48,27 +57,37 @@ class App(TkinterDnD.Tk):
 
         reader = PdfReader(file_path)
         lastPage = False
-        for pageNum in range(len(reader.pages)):
+        skipPage = False
+        toRemove = ["Copyright ©", "Test "]
+        for pageNum in range(len(reader.pages)):        #GET ANSWERS
             page = reader.pages[pageNum]
             if lastPage:
-                break
-            if "100. " in page.extract_text():
-                lastPage = True
-            
-            if pageNum == 0:
-                pass
+                if "1. " not in page.extract_text() and skipPage:
+                    skipPage = False
+                    pass
+                else:
+                    answers_text = "\n".join(
+                        line for line in page.extract_text().splitlines()
+                        if not any(p in line for p in toRemove))
+                    AnswersText += answers_text
             else:
-                toRemove = ["Copyright ©", "Test "]
-                page_text = "\n".join(
-                    line for line in page.extract_text().splitlines()
-                    if not any(p in line for p in toRemove)
-                )
-
-                FinalText += page_text
+                if "100. " in page.extract_text():
+                    lastPage = True
+                    skipPage = True
+            
+                if pageNum == 0:
+                    pass
+                else:
+                    page_text = "\n".join(
+                        line for line in page.extract_text().splitlines()
+                        if not any(p in line for p in toRemove))
+                    
+                    FinalText += page_text
 
         file_name = os.path.basename(file_path)
         self.label.config(text=file_name)
-        self.copy_button.pack(pady=10)
+        self.copy_button.pack(side=tk.LEFT, pady=5)
+        self.copy_answers_button.pack(side=tk.RIGHT)
 
 if __name__ == "__main__":
     app = App()
